9f1a26c5d046d470233fb0842447a7dcc92446d9
TalBenishti <benishti.tal@gmail.com>
2024-03-13 08:53:47 +0200

    new component household-task

---
+++ src/app/components/household-task-assignment/household-task-assignment.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-household-task-assignment',
+  standalone: true,
+  imports: [],
+  templateUrl: './household-task-assignment.component.html',
+  styleUrl: './household-task-assignment.component.css'
+})
+export class HouseholdTaskAssignmentComponent {
+
+}
