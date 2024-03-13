18da868284e70e9eaba5d27a938cf4527af8566a
TalBenishti <benishti.tal@gmail.com>
2024-03-13 12:55:40 +0200

    parson card list component

--- src/app/components/person-card-list/person-card-list.component.ts
+++ src/app/components/person-card-list/person-card-list.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-person-card-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './person-card-list.component.html',
   styleUrl: './person-card-list.component.css'
 })
 export class PersonCardListComponent {
+allPersons: any;
 
 }
