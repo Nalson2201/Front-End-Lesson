0f4161a53621f267ebefe49217204732f1c8165e
TalBenishti <benishti.tal@gmail.com>
2024-03-13 10:40:09 +0200

    person card list component

---
+++ src/app/components/person-card-list/person-card-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person-card-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './person-card-list.component.html',
+  styleUrl: './person-card-list.component.css'
+})
+export class PersonCardListComponent {
+
+}
